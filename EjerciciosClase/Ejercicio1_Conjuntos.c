54116eeab20e25f42a3457aa02a30d3723ec9dad
Andrés González <a324846@alumnos.uaslp.mx>
2022-05-05 09:39:36 -0500

    05/05/2022

---
+++ EjerciciosClase/Ejercicio1_Conjuntos.c
@@ -0,0 +1,22 @@
+/*
+1. Es un grupo de elementos que no se repite
+2. Operaciones:
+   2.1 Agregar al conjunto
+   2.2 Eliminar del conjunto
+   2.3 Pertenencia (elemento x pertenece al conjunto)
+
+3. Operaciones entre conjuntos:
+   3.1 Calcular su interseccion (valores comunes)
+   3.2 Union (todos los elementos de A y B sin repetidos)
+   3.3 Complemento (complemento de A, todos los elementos qu eno aparezcan en A)
+   3.4 Diferencia (A-B todo lo que esta en A que no esta en B)
+
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
